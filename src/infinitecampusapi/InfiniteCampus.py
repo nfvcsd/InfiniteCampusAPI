56dc7f421731bfce9c605865f2e97006a4f05876
Alex Snyder <28544457+TheFuzzyTech@users.noreply.github.com>
2025-04-01 13:06:11 -0500

    Added Teachers, Schools, Doc Strings, and Type hinting

--- src/infinitecampusapi/InfiniteCampus.py
+++ src/infinitecampusapi/InfiniteCampus.py
@@ -2,10 +2,14 @@ from .classes.auth import Auth
 from .classes.Students import Students
 from .classes.Student import Student
 from .classes.Teachers import Teachers
+from .classes.Schools import Schools
 import requests
 
 
 class InfiniteCampus:
+    """Python Object for Interacting with Infinite Campus.
+    Requires token_url, key, secret, and base_url"""
+
     access_token: str
     url: str
 
@@ -21,6 +25,7 @@ class InfiniteCampus:
         self.students = Students(api_call=self.api_call)
         self.student = Student(api_call=self.api_call)
         self.teachers = Teachers(api_call=self.api_call)
+        self.schools = Schools(api_call=self.api_call)
 
     def api_call(self, endpoint):
         token = self.access_token
@@ -28,4 +33,7 @@ class InfiniteCampus:
         r = requests.get(f"{self.url}{endpoint}?limit=5000", headers=headers)
         if r.status_code != 200:
             print(f"API Call returned {r.status_code} status")
+            raise Exception(
+                f"API endpoint {endpoint} returned a {r.status_code} Status Code."
+            )
         return r.json()
